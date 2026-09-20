57784b02b6c9a2e0186b8a9af615b03cfb0c164d
PRIOTOM <ppriotom@gmail.com>
2026-09-20 16:30:20 +0600

    Add files via upload

---
+++ Class 1/YourWork/problem_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int p, q, r, s;
+    char P, Q, R, S;
+    scanf(" %c %d",&P,&p);
+    scanf(" %c %d",&Q,&q);
+    scanf(" %c %d",&R,&r);
+    scanf(" %c %d",&S,&s);
+    printf("%c = %d \n%c = %d\n%c = %d\n%c = %d\n",P,p,Q,q,R,r,S,s);
+    return 0;
+}
